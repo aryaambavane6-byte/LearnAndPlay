9773f9b0038e645c517dcbd1812cd7f2277b3c2e
aryaambavane6-byte <aryaambavane6@gmail.com>
2026-08-30 19:29:37 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,10 @@
-const CACHE_NAME = "learn-play-v4";
+const CACHE_NAME = "learn-play-v5";
 
 const FILES_TO_CACHE = [
+
+    /* =========================
+       HTML FILES
+    ========================= */
 
     "index.html",
     "games.html",
@@ -9,13 +13,51 @@ const FILES_TO_CACHE = [
     "memory.html",
     "color.html",
     "animals.html",
+    "sequence.html",
+
+
+    /* =========================
+       PWA FILES
+    ========================= */
 
     "manifest.json",
 
     "assets/css/style.css",
 
     "assets/icon/icon-192.png",
-    "assets/icon/icon-512.png"
+    "assets/icon/icon-512.png",
+
+
+    /* =========================
+       ABC IMAGES
+    ========================= */
+
+    "assets/images/ABC/apple.jpg",
+    "assets/images/ABC/ball.jpg",
+    "assets/images/ABC/cow.jpg",
+    "assets/images/ABC/dog.jpg",
+    "assets/images/ABC/elephant.jpg",
+    "assets/images/ABC/fish.jpg",
+    "assets/images/ABC/house.jpg",
+    "assets/images/ABC/icecream.jpg",
+    "assets/images/ABC/juice.jpg",
+    "assets/images/ABC/mango.jpg",
+
+
+    /* =========================
+       ANIMAL IMAGES
+    ========================= */
+
+    "assets/images/Animal/cat.jpg",
+    "assets/images/Animal/cow.jpg",
+    "assets/images/Animal/dog.jpg",
+    "assets/images/Animal/elephant.jpg",
+    "assets/images/Animal/giraffe.jpg",
+    "assets/images/Animal/lion.jpg",
+    "assets/images/Animal/monkey.jpg",
+    "assets/images/Animal/panda.jpg",
+    "assets/images/Animal/rabbit.jpg",
+    "assets/images/Animal/tiger.jpg"
 
 ];
 
